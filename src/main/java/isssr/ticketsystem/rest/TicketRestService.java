9e37af78b20487604abb7d857119522694d26c3c
Alessio Di Luzio <alessio.diluzio94@gmail.com>
2018-06-07 18:24:08 +0200

    adeguamento macchina a stati

--- src/main/java/isssr/ticketsystem/rest/TicketRestService.java
+++ src/main/java/isssr/ticketsystem/rest/TicketRestService.java
@@ -97,7 +97,6 @@ public class TicketRestService {
             return new ResponseEntity<>(updatedTicket,HttpStatus.OK);
         else
             return new ResponseEntity<>(updatedTicket,HttpStatus.NOT_FOUND);
-
     }
 
     /**
